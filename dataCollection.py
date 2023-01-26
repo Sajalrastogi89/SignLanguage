6d99793dca7946f0a1d6bce06c24e56b7d12d985
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2023-01-26 18:04:40 +0530

    Image placed on white screen

--- dataCollection.py
+++ dataCollection.py
@@ -4,14 +4,19 @@ import numpy as np
 capture = cv2.VideoCapture(0)
 detect = HandDetector(maxHands=1)
 space = 30
+size=400
 while True:
     result, img = capture.read()
     hands, img = detect.findHands(img)
+    white = np.ones((size,size,3),np.uint8)*255
     if hands:
         x, y, w, h = hands[0]['bbox']
         crop = img[y-space:y+h+space, x-space:x+w+space]
+        white[0:crop.shape[0], 0:crop.shape[1]] = crop
+        print(crop.shape[0],y+h+space,y-space,h)
         cv2.imshow("crop", crop)
-    # white=np.zeros()
+        cv2.imshow("white", white)
+
     # cv2.imshow("White screen",white)
 
 
